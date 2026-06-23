be228f7608a002cd4d0cf1b2736eda2f1815c768
Cremefreesh <archieeknight@gmail.com>
2026-06-23 02:04:32 +0100

    Backend Websocket

---
+++ backend/app/api/routes/websocket.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+
+router = APIRouter()
+
+active_connections = []
+
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    active_connections.append(websocket)
+
+    try:
+        while True:
+            message = await websocket.receive_text()
+
+            for connection in active_connections:
+                await connection.send_text(message)
+
+    except WebSocketDisconnect:
+        active_connections.remove(websocket)
